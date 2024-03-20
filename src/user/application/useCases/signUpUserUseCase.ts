cd81d813d34fe874c1ccce207a389b25da8c3dbf
Gsuscrist <jago2811@gmail.com>
2024-03-20 17:55:05 -0600

    [add] signUpUseCase

---
+++ src/user/application/useCases/signUpUserUseCase.ts
@@ -0,0 +1,22 @@
+import {UserRepository} from "../../domain/repository/userRepository";
+import {Credentials} from "../../domain/credentials";
+
+export class SignUpUserUseCase{
+    constructor(readonly repository:UserRepository) {
+    }
+
+    async run(
+        uuid: string,
+        name: string,
+        lastname: string,
+        credentials: Credentials,
+        role: "admin"| "user",
+    ):Promise<any>{
+        try {
+            return await this.repository.signUp(uuid,name,lastname,credentials,role)
+        }catch (e) {
+            console.log(e)
+            return null
+        }
+    }
+}
