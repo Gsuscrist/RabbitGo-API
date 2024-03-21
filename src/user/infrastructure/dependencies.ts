103957d593ca74939dd9113d32f92a37bf4323be
Gsuscrist <jago2811@gmail.com>
2024-03-20 18:33:52 -0600

    [add] logIn dependencies

--- src/user/infrastructure/dependencies.ts
+++ src/user/infrastructure/dependencies.ts
@@ -3,13 +3,19 @@ import {BCryptService} from "../domain/service/bCryptService";
 import {SignUpUserUseCase} from "../application/useCases/signUpUserUseCase";
 import {GenerateUuidUserUseCase} from "../application/useCases/generateUuidUserUseCase";
 import {SignUpUserController} from "./controllers/signUpUserController";
+import {LoginUserUseCase} from "../application/useCases/loginUserUseCase";
+import {LoginUserController} from "./controllers/loginUserController";
+import {Jwt} from "../application/jwt/jwt";
 
 
 export const database = new MysqlUserRepository()
 export const encryptService = new BCryptService()
-
+export const jwt = new Jwt()
 
 export const signUpUserUseCase = new SignUpUserUseCase(database)
 export const generateUuid = new GenerateUuidUserUseCase(database)
 export const  signUpUserController = new SignUpUserController(signUpUserUseCase,encryptService,generateUuid)
 
+
+export const logInUserUseCase = new LoginUserUseCase(database)
+export const logInUserController = new LoginUserController(logInUserUseCase,encryptService,jwt)
