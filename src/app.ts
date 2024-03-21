06036b1fc8544b4d2bd649424d9eecc4d675666d
Gsuscrist <jago2811@gmail.com>
2024-03-20 18:15:42 -0600

    [add] userRoute

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import {Signale} from 'signale';
+import {userRoute} from "./user/infrastructure/route/userRoute";
 
 const app = express();
 const signale = new Signale();
 
 app.use(express.json())
 
+app.use('/user', userRoute);
 
 app.listen(8080,()=>{
     signale.success("Server on line in port: 8080")
