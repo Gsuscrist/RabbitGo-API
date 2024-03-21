488207ad55fff5428cbe90ed718002ac076dda79
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2024-03-20 21:49:07 -0600

    [add] user findById userRoute

--- src/user/infrastructure/route/userRoute.ts
+++ src/user/infrastructure/route/userRoute.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import {logInUserController, signUpUserController} from "../dependencies";
+import {getUserController, logInUserController, signUpUserController} from "../dependencies";
 
 
 export const userRoute = express.Router();
 
 userRoute.post("/", signUpUserController.run.bind(signUpUserController))
 userRoute.post("/login", logInUserController.run.bind(logInUserController))
+userRoute.get("/:uuid", getUserController.run.bind(getUserController))
