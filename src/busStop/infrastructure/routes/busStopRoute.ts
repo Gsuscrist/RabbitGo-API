152d374a412affb42dee07cdcc38ab63b53e4302
Reus <victor.villalva2003@hotmail.com>
2024-03-28 14:36:20 -0600

    [ADD] busStop put infrastructure routes

--- src/busStop/infrastructure/routes/busStopRoute.ts
+++ src/busStop/infrastructure/routes/busStopRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import {authenticateMiddleware} from "../../../middleware/authenticator";
-import {createBusStopController} from "../dependencies";
+import {createBusStopController, updateBusStopController} from "../dependencies";
 
 
 export const busStopRouter = express.Router()
 
 
 busStopRouter.post("/",authenticateMiddleware,createBusStopController.run.bind(createBusStopController))
+busStopRouter.put("/:uuid",authenticateMiddleware,updateBusStopController.run.bind(updateBusStopController))
