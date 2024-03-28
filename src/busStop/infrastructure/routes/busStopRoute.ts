6fa781e1d608947509c550842207cc99360e3d33
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2024-03-28 13:06:40 -0600

    [add] busStop getByUuid routes

--- src/busStop/infrastructure/routes/busStopRoute.ts
+++ src/busStop/infrastructure/routes/busStopRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import {authenticateMiddleware} from "../../../middleware/authenticator";
-import {createBusStopController} from "../dependencies";
+import {createBusStopController, getBusStopController} from "../dependencies";
 
 
 export const busStopRouter = express.Router()
 
 
 busStopRouter.post("/",authenticateMiddleware,createBusStopController.run.bind(createBusStopController))
+busStopRouter.get("/:uuid",authenticateMiddleware,getBusStopController.runById.bind(getBusStopController))
