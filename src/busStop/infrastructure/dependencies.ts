6d0f8364918f021c8f0e4a95fd620687a655d34e
Reus <victor.villalva2003@hotmail.com>
2024-03-28 14:34:05 -0600

    [ADD] updateBusStopUseCase updateBusStopController infrastructure dependencies

--- src/busStop/infrastructure/dependencies.ts
+++ src/busStop/infrastructure/dependencies.ts
@@ -2,6 +2,8 @@ import {MysqlBusStopRepository} from "./repository/mysqlBusStopRepository";
 import {CreateBusStopUseCase} from "../application/useCase/createBusStopUseCase";
 import {GenerateUuidBusStopUseCase} from "../application/useCase/generateUuidBusStopUseCase";
 import {CreateBusStopController} from "./controllers/createBusStopController";
+import { UpdateBusStopUseCase } from "busStop/application/useCase/updateBusStopUseCase";
+import { UpdateBusStopController } from "./controllers/updateBusStopController";
 
 
 export const mysqlBusStopRepository = new MysqlBusStopRepository()
@@ -9,3 +11,6 @@ export const mysqlBusStopRepository = new MysqlBusStopRepository()
 export const createBusStopUseCase = new CreateBusStopUseCase(mysqlBusStopRepository)
 export const generateUuid = new GenerateUuidBusStopUseCase(mysqlBusStopRepository)
 export const createBusStopController = new CreateBusStopController(createBusStopUseCase,generateUuid)
+
+export const updateBusStopUseCase = new UpdateBusStopUseCase(mysqlBusStopRepository)
+export const updateBusStopController = new UpdateBusStopController(updateBusStopUseCase)
