2b0f0b63e7970ad773bb25dbeb85a51bfab3ebc2
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2024-03-28 12:33:19 -0600

    [add] busStop getByUuid controller dependencies

---
+++ src/busStop/infrastructure/controllers/getBusStopController.ts
@@ -0,0 +1,33 @@
+import {Request,Response} from "express";
+import {GetBusStopUseCase} from "../../application/useCase/getBusStopUseCase";
+
+
+export class GetBusStopController{
+    constructor(readonly usecase:GetBusStopUseCase) {
+    }
+
+    async runById(req:Request,res:Response){
+        try {
+            let uuid = req.params.uuid
+            const busStop = await this.usecase.runByUuid(uuid)
+            if (busStop){
+                return res.status(200).send({
+                    status:"success",
+                    data:busStop,
+                    message:"bus stop got successfully"
+                })
+            }
+            res.status(400).send({
+                status:"error",
+                data:[],
+                message:"bus stop got failed "
+            })
+        }catch (e) {
+            console.log("controller:\n",e)
+            res.status(417).send({
+                message:"error",
+                error: e
+            })
+        }
+    }
+}
--- src/busStop/infrastructure/dependencies.ts
+++ src/busStop/infrastructure/dependencies.ts
@@ -2,6 +2,8 @@ import {MysqlBusStopRepository} from "./repository/mysqlBusStopRepository";
 import {CreateBusStopUseCase} from "../application/useCase/createBusStopUseCase";
 import {GenerateUuidBusStopUseCase} from "../application/useCase/generateUuidBusStopUseCase";
 import {CreateBusStopController} from "./controllers/createBusStopController";
+import { GetBusStopController } from "./controllers/getBusStopController";
+import { GetBusStopUseCase } from "busStop/application/useCase/getBusStopUseCase";
 
 
 export const mysqlBusStopRepository = new MysqlBusStopRepository()
@@ -9,3 +11,6 @@ export const mysqlBusStopRepository = new MysqlBusStopRepository()
 export const createBusStopUseCase = new CreateBusStopUseCase(mysqlBusStopRepository)
 export const generateUuid = new GenerateUuidBusStopUseCase(mysqlBusStopRepository)
 export const createBusStopController = new CreateBusStopController(createBusStopUseCase,generateUuid)
+
+export const getBusStopUseCase = new GetBusStopUseCase(mysqlBusStopRepository)
+export const getBusStopController = new GetBusStopController(getBusStopUseCase)
