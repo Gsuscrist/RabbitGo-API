06c126410acf334126b61b52a4192ef9bcc55948
Reus <victor.villalva2003@hotmail.com>
2024-04-07 23:42:21 -0600

    [ADD] getPathUseCase getPathController infrastructure dependencies

--- src/path/infrastructure/dependencies.ts
+++ src/path/infrastructure/dependencies.ts
@@ -2,6 +2,8 @@ import {MysqlPathRepository} from "./repository/mysqlPathRepository";
 import {CreatePathUseCase} from "../application/useCases/createPathUseCase";
 import {GenerateUuidPathUseCase} from "../application/useCases/generateUuidPathUseCase";
 import {CreatePathController} from "./controllers/CreatePathController";
+import { GetPathUseCase } from "path/application/useCases/getPathUseCase";
+import { GetPathController } from "./controllers/getPathController";
 
 export const mysqlPathRepository = new MysqlPathRepository()
 
@@ -9,3 +11,5 @@ export const mysqlPathRepository = new MysqlPathRepository()
 export const createPathUseCase = new CreatePathUseCase(mysqlPathRepository)
 export const generateUuid = new GenerateUuidPathUseCase(mysqlPathRepository)
 export const createPathController = new CreatePathController(createPathUseCase,generateUuid)
+export const getPathUseCase = new GetPathUseCase(mysqlPathRepository)
+export const getPathController = new GetPathController(getPathUseCase)
