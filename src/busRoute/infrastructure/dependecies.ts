d8e85c014f24696a9bb80ac8c34cde8c4c23969d
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2024-04-07 20:11:51 -0600

    [ADD] busRoute getById routes dependencies

--- src/busRoute/infrastructure/dependecies.ts
+++ src/busRoute/infrastructure/dependecies.ts
@@ -2,9 +2,14 @@ import {MysqlBusRouteRepository} from "./repository/mysqlBusRouteRepository";
 import {CreateBusRouteUseCase} from "../application/useCase/createBusRouteUseCase";
 import {GenerateUuidBusRouteUseCase} from "../application/useCase/generateUuidBusRouteUseCase";
 import {CreateBusRouteController} from "./controllers/createBusRouteController";
+import { GetBusRouteUseCase } from "busRoute/application/useCase/getBusRouteUseCase";
+import { GetBusRouteController } from "./controllers/getBusRouteController";
 
 export const mysqlBusRouteRepository = new MysqlBusRouteRepository()
 
 export const createBusRouteUseCase = new CreateBusRouteUseCase(mysqlBusRouteRepository)
 export const generateUuid = new GenerateUuidBusRouteUseCase(mysqlBusRouteRepository)
 export const createBusRouteController = new CreateBusRouteController(createBusRouteUseCase,generateUuid)
+
+export const getBusRouteUseCase = new GetBusRouteUseCase(mysqlBusRouteRepository)
+export const getBusRouteController = new GetBusRouteController(getBusRouteUseCase)
